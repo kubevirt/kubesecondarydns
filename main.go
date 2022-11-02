99724e55751b87d9fd728a9f3596d48257283f65
Alona Paz <alkaplan@redhat.com>
2022-11-02 08:48:28 +0200

    operator-sdk create api
    operator-sdk create api --group kubevirt.io --version v1 --kind VirtualMachineInstance --controller
    Create Resource [y/n]
    n

--- main.go
+++ main.go
@@ -30,6 +30,8 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/AlonaKaplan/kubesecondarydns/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -85,6 +87,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.VirtualMachineInstanceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "VirtualMachineInstance")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
