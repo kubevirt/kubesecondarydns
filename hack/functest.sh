b1847b3bf217b6d6fc4157c88d27d3f3f1235305
Alona Paz <alkaplan@redhat.com>
2022-12-04 12:42:29 +0200

    Update the functest.sh to invoke the e2e tests
    The script also makes sure `nslookup` is installed.
    
    Signed-off-by: Alona Paz <alkaplan@redhat.com>

--- hack/functest.sh
+++ hack/functest.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
 
-echo hello world
+source ./cluster/cluster.sh
+
+if ! nslookup www.google.com > /dev/null 2>&1; then
+  echo "nslookup error"
+  exit 1
+fi
+
+KUBECONFIG=${KUBECONFIG:-$(cluster::kubeconfig)} $GO test -test.timeout=1h -test.v ./tests/... -ginkgo.v
