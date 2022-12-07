26a3f6c0866407df5248a5c206b375dfea8e0740
Diana Teplits <dteplits@redhat.com>
2022-12-07 10:49:14 +0200

    zone manager: Handle exisitng SOA serial use case
    When zone manager is created, if zone file already exist and contains SOA serial,
    then the next SOA serial should continue the existing one.
    
    Signed-off-by: Diana Teplits <dteplits@redhat.com>

--- pkg/zonemgr/zone_file.go
+++ pkg/zonemgr/zone_file.go
@@ -1,10 +1,16 @@
 package zonemgr
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 const zoneFilePerm = 0644
+
+var soaSerialReg = regexp.MustCompile("SOA .*\\(([0-9]+) ")
 
 type ZoneFile struct {
 	zoneFileFullName string
@@ -18,4 +24,43 @@ func NewZoneFile(fileName string) *ZoneFile {
 
 func (zoneFile *ZoneFile) writeFile(content string) (err error) {
 	return os.WriteFile(zoneFile.zoneFileFullName, []byte(content), zoneFilePerm)
+}
+
+func (zoneFile *ZoneFile) readFile() ([]byte, error) {
+	return os.ReadFile(zoneFile.zoneFileFullName)
+}
+
+func (zoneFile *ZoneFile) isFileExist() (bool, error) {
+	var err error
+	isExist := false
+	if _, err = os.Stat(zoneFile.zoneFileFullName); err == nil {
+		isExist = true
+	} else if errors.Is(err, os.ErrNotExist) {
+		err = nil
+	}
+	return isExist, err
+}
+
+func (zoneFile *ZoneFile) ReadSoaSerial() (*int, error) {
+	if isFileExist, err := zoneFile.isFileExist(); !isFileExist || err != nil {
+		return nil, err
+	}
+	if content, err := zoneFile.readFile(); content == nil || err != nil {
+		return nil, err
+	} else {
+		return fetchSoaSerial(string(content))
+	}
+}
+
+func fetchSoaSerial(content string) (*int, error) {
+	if result := soaSerialReg.FindStringSubmatch(content); result != nil && len(result) > 0 {
+		soaSerial := result[1]
+		if soaSerialInt, err := strconv.Atoi(soaSerial); err == nil {
+			return &soaSerialInt, nil
+		} else {
+			return nil, err
+		}
+	} else {
+		return nil, errors.New(fmt.Sprintf("failed to fetch SOA serial value from the zone file content: %s", content))
+	}
 }
