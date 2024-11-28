17ffe89bc38422ec114f2c36160746bd2d4395f2
Jan Schintag <jan.schintag@de.ibm.com>
2024-11-28 13:04:43 +0100

    install-go.sh: Support installing golang on multiple arches
    Add support for installing golang on arm64 and s390x.
    
    Signed-off-by: Jan Schintag <jan.schintag@de.ibm.com>

--- hack/install-go.sh
+++ hack/install-go.sh
@@ -2,7 +2,26 @@
 
 destination=$1
 version=$(grep "^go " go.mod |awk '{print $2}')
-tarball=go$version.linux-amd64.tar.gz
+
+arch="$(uname -m)"
+
+case $arch in
+    x86_64 | amd64)
+        arch="amd64"
+        ;;
+    aarch64 | arm64)
+        arch="arm64"
+        ;;
+    s390x)
+        arch="s390x"
+        ;;
+    *)
+        echo "ERROR: invalid arch=${arch}, only support x86_64, aarch64 and s390x"
+        exit 1
+        ;;
+esac
+
+tarball=go$version.linux-$arch.tar.gz
 url=https://dl.google.com/go/
 
 mkdir -p $destination
