5fe1a1b48dc777d569e7f2ba880f5500d7e51e89
Enrique Llorente <ellorent@redhat.com>
2025-05-13 11:57:51 +0200

    main, manifests: Add readiness and lifeness probes
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- main.go
+++ main.go
@@ -23,11 +23,13 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	v1 "kubevirt.io/api/core/v1"
@@ -56,8 +58,9 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	ctrlOptions := ctrl.Options{
-		Scheme:             scheme,
-		MetricsBindAddress: "0", // disable metrics
+		Scheme:                 scheme,
+		MetricsBindAddress:     "0",     // disable metrics
+		HealthProbeBindAddress: ":8081", // enable health probes
 	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrlOptions)
@@ -81,6 +84,17 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "VirtualMachineInstance")
 		os.Exit(1)
 	}
+
+	// Add readiness and liveness probes
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
 	//+kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
