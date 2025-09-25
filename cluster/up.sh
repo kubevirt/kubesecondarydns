5b9c3b313182845e39bf4b1566f4ba188eb3c09e
Or Shoval <oshoval@redhat.com>
2025-09-25 13:21:19 +0300

    kubevirtci: Use Flannel
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- cluster/up.sh
+++ cluster/up.sh
@@ -20,6 +20,8 @@ export DEPLOY_CNAO=${DEPLOY_CNAO:-true}
 export DEPLOY_KUBEVIRT=${DEPLOY_KUBEVIRT:-true}
 export KUBEVIRT_PSA=${KUBEVIRT_PSA:-true}
 
+export KUBEVIRT_FLANNEL=true
+
 source ./cluster/cluster.sh
 
 # use kubevirt latest stable release
